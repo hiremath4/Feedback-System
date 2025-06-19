c164ddb9f4f4fb813cdaf3be88d6377703c7157f
SUMIT HIREMATH <123145236+hiremath4@users.noreply.github.com>
2025-06-19 15:29:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from src.sentiment_analysis import analyze_sentiment
 
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
 @app.route('/feedback-system-oedg.onrender.com')
 def home():
     return render_template('index.html')

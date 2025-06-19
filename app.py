55b89b12a0e37541c25a760c27a388609068d583
SUMIT HIREMATH <123145236+hiremath4@users.noreply.github.com>
2025-06-19 15:38:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
 
-@app.route('/feedback-system-oedg.onrender.com')
+@app.route('/')
 def home():
     return render_template('index.html')
 
